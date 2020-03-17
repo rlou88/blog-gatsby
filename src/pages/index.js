149a7d8222e2ddf911fd81f43dd92c15b881f26e
Ronald <ronald.lou88@gmail.com>
2020-03-17 13:54:31 -0500

    updated solution

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import Layout from '../components/layout';
+import Hero from '../components/hero';
+import Insta from '../components/insta';
+import PostPreview from '../components/post-preview';
+import usePosts from '../hooks/use-posts';
 
-export default () => (
-  <>
-    <h1>Home</h1>
-    <p>Hello San Antonio!</p>
-    <Link to="/about/"> Learn about me &rarr;</Link>
-  </>
-);
+export default () => {
+  const posts = usePosts();
+
+  return (
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read my blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+        <Insta />
+      </Layout>
+    </>
+  );
+};
